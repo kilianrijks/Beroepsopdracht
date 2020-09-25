50e4ec9ef569e2e3b7b36e4fa8e14b24670ac33b
Kilian Rijks <coolkil321@gmail.com>
2020-09-25 15:16:05 +0200

    Added and modified files

--- ditbenik.py
+++ ditbenik.py
@@ -36,31 +36,54 @@ while True:
 
   if ( answerq2 == 'a' ):
     print("Dit antwoord is fout! Ik zat op het clusius")
+    answerq2way = True
 
   if ( answerq2 == 'b' ):
     print("Dit antwoord is fout! Ik zat op het clusius")
+    answerq2way = True
 
-  if (answerq2 == 'c' ):
+  if ( answerq2 == 'c' ):
     print("Dit is het goeie antwoord!")
+    answerq2way = False
+  
+    
+  if ( answerq2way ==  False ):
+    print("Laatste vraag")
+    print("Hoe oud ben ik?")
 
-  print("Laatste vraag")
-  print("Hoe oud ben ik?")
+    print("a 16")
+    print("b 17")
+    print("c 18")
 
-  print("a 16")
-  print("b 17")
-  print("c 18")
+    answerq3 = input()
 
-  answerq3 = input()
+    if ( answerq3 == 'a' ):
+        print("Dit antwoord is goed!")
 
-  if ( answerq3 == 'a' ):
-    print("Dit antwoord is goed!")
+    if ( answerq3 == 'b'):
+        print("Dit antwoord is fout! Ik ben 16")
 
-  if ( answerq3 == 'b'):
-    print("Dit antwoord is fout! Ik ben 16")
+    if ( answerq3 == 'c' ):
+        print("Dit antwoord is fout! Ik ben 16")
 
-  if ( answerq3 == 'c' ):
-    print("Dit antwoord is fout! Ik ben 16")
-        
+  if ( answerq2way == True ):
+    print("Goed gedaan! je wist het antwoord niet op vraag 2 en daardoor heb je een secret way gevonden")
+    print("Wat word je antwoord a of b?")
+
+    print("a Failure")
+    print("b Failure")
+
+    answerq4 = input()
+
+
+    if ( answerq4 == 'a'):
+      print("Je hebt gefaald op vraag 2 nu krijg je de secret bad ending")
+
+    if ( answerq4 == 'b'):
+      print("Je hebt gefaald op vraag 2 nu krijg je de secret bad ending")
+      
+  if ( answerq2way == False ):
+    print("Je krijgt de good ending, maar er is ook nog een bad ending...") 
   print("Wil je dit programma nog een keer doen? Type y/n")
   answer = input()
 
